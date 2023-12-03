8268dbbe1bde010eb4f5d56988ded00deff4847a
Allan Rizza <allanrizza.dev@gmail.com>
2023-12-03 18:47:56 -0300

    disabling csrf security

--- src/main/java/com/simpleblog/config/WebSecurityConfig.java
+++ src/main/java/com/simpleblog/config/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().permitAll()
                 );
