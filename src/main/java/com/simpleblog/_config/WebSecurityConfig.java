85ef660441cfb4948fafd8847a52b7e125437ba0
Allan Rizza <allanrizza.dev@gmail.com>
2023-12-12 23:32:21 -0300

    Changing security configs

--- src/main/java/com/simpleblog/_config/WebSecurityConfig.java
+++ src/main/java/com/simpleblog/_config/WebSecurityConfig.java
@@ -2,52 +2,26 @@ package com.simpleblog._config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable()
-                .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().permitAll()
-                );
-        return http.build();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(
+                authorizateConfig -> {
+                    authorizateConfig.requestMatchers("/public").permitAll();
+                    authorizateConfig.requestMatchers("/logout").permitAll();
+                    authorizateConfig.anyRequest().authenticated();
+                }
+            )
+            .formLogin(Customizer.withDefaults())
+            .build();
     }
 
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/", "/home").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin((form) -> form
-//                        .loginPage("/login")
-//                        .permitAll()
-//                )
-//                .logout((logout) -> logout.permitAll());
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
 }
